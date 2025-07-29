7dd67fcd1381cc1a980dce180a11f1f1c225beb7
CarloC <carlo.casiglia@develhope.co>
2025-07-29 17:45:38 +0200

    fix

--- app/db.py
+++ app/db.py
@@ -23,14 +23,15 @@ class DBWriter:
                          p4_tot_wh,
                          tot_wh):
         point = Point("game_status") \
-            .field("remaining_secs", remaining_secs) \
-            .field("p1_w", p1_w) \
-            .field("p2_w", p2_w) \
-            .field("p3_w", p3_w) \
-            .field("p4_w", p4_w) \
-            .field("p1_tot_wh", p1_tot_wh) \
-            .field("p2_tot_wh", p2_tot_wh) \
-            .field("p3_tot_wh", p3_tot_wh) \
-            .field("p4_tot_wh", p4_tot_wh) \
-            .field("tot_wh", tot_wh)
+            .field("remaining_secs", float(remaining_secs)) \
+            .field("p1_w", float(p1_w)) \
+            .field("p2_w", float(p2_w)) \
+            .field("p3_w", float(p3_w)) \
+            .field("p4_w", float(p4_w)) \
+            .field("p1_tot_wh", float(p1_tot_wh)) \
+            .field("p2_tot_wh", float(p2_tot_wh)) \
+            .field("p3_tot_wh", float(p3_tot_wh)) \
+            .field("p4_tot_wh", float(p4_tot_wh)) \
+            .field("tot_wh", float(tot_wh))
+
         self.write_api.write(bucket=self.bucket, record=point)
